a9cc898b0903f94e01c89e371f35da6457f89c2f
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-06-07 21:46:57 -0300

    Atualizando analisador de numeros.js

--- analisador de numeros/index.js
+++ analisador de numeros/index.js
@@ -1,118 +1,112 @@
-// let num = document.querySelector("input#number")
-// let tab = document.querySelector("select#tab")
-// let res = document.querySelector("div#res")
-// let valores = []
+let num = document.querySelector("input#number")
+let tab = document.querySelector("select#tab")
+let res = document.querySelector("div#res")
+let valores = []
 
-// function isNumero(num) {
+function isNumero(num) {
 
-//     if( Number(num) >= 1 && Number(num) <= 100 ) {
+    if( Number(num) >= 1 && Number(num) <= 100 ) {
 
-//         return true
+        return true
 
-//     } else {
+    } else {
 
-//         return false
-//     }
-// }
+        return false
+    }
+}
 
-// function inLista(num, list) {
+function inLista(num, list) {
 
-//     if( list.indexOf(Number(num)) != -1 ) {
+    if( list.indexOf(Number(num)) != -1 ) {
 
-//         return true 
+        return true 
 
-//     } else {
+    } else {
 
-//         return false
+        return false
         
-//  }
-// }
+ }
+}
 
-// function adicionar() {
+function adicionar() {
 
-//     let numb = num.value 
+    let numb = num.value 
 
-//     if( isNumero(numb) && !inLista(numb, valores )) {
+    if( isNumero(numb) && !inLista(numb, valores )) {
 
-//         valores.push(Number(numb))
+        valores.push(Number(numb))
         
-//         let item = document.createElement("option")  
-//         item.innerHTML = `O valor ${numb} adicionado` 
-//         tab.appendChild(item)
-//         res.innerHTML = ``
+        let item = document.createElement("option")  
+        item.innerHTML = `O valor ${numb} adicionado` 
+        tab.appendChild(item)
+        res.innerHTML = ``
         
-//     } else {
+    } else {
         
-//         window.alert('Valor inválido ou já encontrado na lista')
+        window.alert('Valor inválido ou já encontrado na lista')
         
-//     }
+    }
 
-//     num.value = ''
-//     num.focus()
+    num.value = ''
+    num.focus()
     
-// }
+}
 
-// function finalizar() {
+function finalizar() {
 
-//     if( valores.length == 0 ){
+    if( valores.length == 0 ){
 
-//         window.alert('Adicione valores antes de finalizar')
+        window.alert('Adicione valores antes de finalizar')
 
-//     } else {
+    } else {
 
-//         let total = valores.length
-//         let maior = valores[0]
-//         let menor = valores[0]
-//         let soma = 0
-//         let media = 0
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
 
-//         for(let posicao in valores) {
-//             if( valores[posicao] > maior ) {
-//                 maior =  valores[posicao]
-//             }
-//             if( valores[posicao] < menor ) {
-//                 menor = valores[posicao]
-//             }
-//             soma += valores[posicao]
-//             media = soma / total
-//         }
+        for(let posicao in valores) {
+            if( valores[posicao] > maior ) {
+                maior =  valores[posicao]
+            }
+            if( valores[posicao] < menor ) {
+                menor = valores[posicao]
+            }
+            soma += valores[posicao]
+            media = soma / total
+        }
 
-//         res.innerHTML = ``
+        res.innerHTML = ``
 
-//         if( total <= 1 )  {
+        if( total <= 1 )  {
 
-//         res.innerHTML += `
-//         <p>Ao todo, temos ${total} número cadastrados.</p>
-//         `
+        res.innerHTML += `
+        <p>Ao todo, temos ${total} número cadastrados.</p>
+        `
 
-//         }else {
+        }else {
 
-//         res.innerHTML += `
-//         <p>Ao todo, temos ${total} números cadastrados.</p>
-//         `
-//         res.innerHTML +=`
-//         <p>O maior valor informado foi ${maior}.</p>
-//         `
-//         res.innerHTML +=`
-//         <p>O menor valor informado foi ${menor}.</p>
-//         `
-//         res.innerHTML +=`
-//         <p>A soma dos valores foi ${soma}.</p>
-//         `
-//         res.innerHTML +=`
-//         <p>A média dos valores foi ${media}.</p>
-//         `
-//         }
-//     }
-// }
+        res.innerHTML += `
+        <p>Ao todo, temos ${total} números cadastrados.</p>
+        `
+        res.innerHTML +=`
+        <p>O maior valor informado foi ${maior}.</p>
+        `
+        res.innerHTML +=`
+        <p>O menor valor informado foi ${menor}.</p>
+        `
+        res.innerHTML +=`
+        <p>A soma dos valores foi ${soma}.</p>
+        `
+        res.innerHTML +=`
+        <p>A média dos valores foi ${media}.</p>
+        `
+        }
+    }
+}
         
-num = 8 
-num **= 2 
-console.log(num)
 
-n = 1543.5
-console.log( n.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'}))
-console.log( n.toFixed('pt-BR', {style:'currency', currency:'BRL'}))
 
 
 
